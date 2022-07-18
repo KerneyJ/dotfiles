139ca20a435666433d5dac6c8fb34364c2a9e42e
kerneyj <gamerguy9801@gmail.com>
2022-07-18 01:52:19 -0500

    updated start commands

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 pulseaudio --start
-xinput set-prop 14 338 1 # tap clicking
-xinput set-prop 14 317 1 # natural scrolling
-xinput set-prop 15 338 1 # tap clicking
-xinput set-prop 15 317 1 # natural scrolling
+xinput --set-prop 14 338 1 # tap clicking
+xinput --set-prop 14 317 1 # natural scrolling
+xinput --set-prop 15 338 1
+xinput --set-prop 15 317 1
 picom -b --experimental-backends
 nitrogen --restore
